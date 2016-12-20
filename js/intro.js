9e1343e28bdf7e2fd896c4924e550fa6258817c2
Sacha Bobinet <sachabobinet@gmail.com>
2016-12-21 00:12:25 +0100

    Premiere ebauche intro

--- js/intro.js
+++ js/intro.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function(){ // Appel via jQuery
 
 // Initiation controlleur ScrollMagic 
@@ -22,3 +23,75 @@ var Intro = new ScrollMagic.Scene({
 
 
     })(jQuery);
+*/
+$(document).ready(function(){ // Appel via jQuery
+
+// Initiation controlleur ScrollMagic
+
+    var controller = new ScrollMagic.Controller();
+
+
+
+
+
+    var IntroTween = TweenMax.from("#planète, #fusée-seule, #personne", 1, {scale: 0.7, ease: Expo.easeOut
+    });
+    var Intro = new ScrollMagic.Scene({
+        triggerElement: ".svg-wrapper",
+        duration: 300,
+        triggerHook: 0.2,
+    })
+        .setTween(IntroTween)
+        .addIndicators({name:"Scale Élements"})
+        .addTo(controller);
+
+
+
+    var TitleFadeIn = TweenMax.from(".titles-wrapper", 7, {autoAlpha: 0
+    });
+    var TitleFadeInScene = new ScrollMagic.Scene({
+        triggerElement: "#titles-wrapper",
+        duration: 400,
+        triggerHook: 0.2,
+    })
+        .setTween(TitleFadeIn)
+        .addTo(controller);
+
+
+
+    var FuséeTL= TweenMax.from(".droite-fusée", 7, {x: +200, autoAlpha: 0});
+    var FuséeTLScene = new ScrollMagic.Scene({
+        triggerElement: '.fusee',
+        triggerHook: 0.6,
+        duration: 500
+    })
+        .setTween(FuséeTL)
+        .addIndicators()
+        .addTo(controller);
+
+    var FuséeTL= TweenMax.from(".gauche-fusée", 7, {x: -200, autoAlpha: 0});
+    var FuséeTLScene = new ScrollMagic.Scene({
+        triggerElement: '.fusee',
+        triggerHook: 0.6,
+        duration: 500
+    })
+        .setTween(FuséeTL)
+        .addIndicators()
+        .addTo(controller);
+
+    var FuséeTL= TweenMax.from(".haut-fusée", 7, {y: -200, autoAlpha: 0});
+    var FuséeTLScene = new ScrollMagic.Scene({
+        triggerElement: '.fusee',
+        triggerHook: 0.6,
+        duration: 500
+    })
+        .setTween(FuséeTL)
+        .addIndicators()
+        .addTo(controller);
+
+
+
+
+
+
+})(jQuery);
